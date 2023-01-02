ef63f1a1aa6c1d35a638ffd00c7d9b3cc4e401c2
Bao <vuthebao773@gmail.com>
2023-01-02 15:44:28 +0700

    #11_Skill short variable code

--- Hoi_Dan_IT.js
+++ Hoi_Dan_IT.js
@@ -1,7 +1,13 @@
-// let stringPro = "adjkshfkjashfjkhaskjfhks";
-// console.log(stringPro.length);
+// skill short variable code
+let a = 10;
+let b = 20;
+console.log(`a + b = ${a + b}`); 
 
-console.log(`chao ae`.toUpperCase());
+let variA = 50;
+let variB = 50;
+console.log(`Combine all = ${variA + variB}`);
 
-let a = +"1234567"; // convert (String) to (Number) "use: +";
-console.log(typeof a);
+let dog = "My Dog";
+let cat = "My Cat";
+console.log(`My House + ${dog} + ${cat}`);
+console.log(`My House + ${dog + cat}`);
