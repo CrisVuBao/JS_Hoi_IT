cd04efb645676e3718f415786609af385f70d15f
Bao <vuthebao773@gmail.com>
2023-01-02 14:21:00 +0700

    file demo

--- Hoi_Dan_IT.js
+++ Hoi_Dan_IT.js
@@ -1,8 +1,2 @@
-let hoiDanIT = "Xin chào IT";
-console.log(hoiDanIT);
-
-let hoiPro = "Chào";
-console.log(hoiPro);
-
-let fixSuccess = "Đã Fix thành công";
-console.log(fixSuccess);
+let stringPro = "adjkshfkjashfjkhaskjfhks";
+console.log(stringPro.length);
