74959bc013c9dcf2363f2e5d357c4c9063167c96
Bao <vuthebao773@gmail.com>
2023-01-03 14:42:27 +0700

    Video_#12 Oject

--- Hoi_Dan_IT.js
+++ Hoi_Dan_IT.js
@@ -1,22 +1,6 @@
-// skill short variable code
-// use backticks(dấu nháy chéo) Ex: ${expresstion}
-let a = 10;
-let b = 20;
-console.log(`a + b = ${a + b}`); 
-
-let variA = 50;
-let variB = 50;
-console.log(`Combine all = ${variA + variB}`);
-
-let dog = "My Dog";
-let cat = "My Cat";
-console.log(`My House + ${dog} + ${cat}`);
-console.log(`My House + ${dog + cat}`);
-
-let proVip = "This is a Ex about Backticks"
-console.log(`ProVip: ${proVip}`);
-
+//Oject
 let person = {
+//  key: value
     name: "tao pro",
     age: "17",
     favorite: "like Kpop"
@@ -24,5 +8,5 @@ let person = {
     
 }
 
-person.age = 100;
+person.name = "vip" // when creating more 1 new key (name), value will changed
 console.log(person);
