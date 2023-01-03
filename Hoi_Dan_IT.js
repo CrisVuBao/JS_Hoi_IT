aa57b0c7ba779b48618e018710aabe6a6946e7c7
Bao <vuthebao773@gmail.com>
2023-01-03 15:06:31 +0700

    Video_#13 Array

--- Hoi_Dan_IT.js
+++ Hoi_Dan_IT.js
@@ -1,12 +1,25 @@
-//Oject
-let person = {
-//  key: value
-    name: "tao pro",
-    age: "17",
-    favorite: "like Kpop"
-    
-    
-}
+// Array = [item1,item2,item3,....];
+//Ex Output: 
 
-person.name = "vip" // when creating more 1 new key (name), value will changed
-console.log(person);
+/*[
+    0: "A", 0: is index
+    1: "B", 1: is index
+    2: "C"  2: is index
+  ]*/
+
+  // Oject in Array
+  //Ex 1:
+  let computer = [
+    {name: "PC", display: "100inh"},
+    {name: "Laptop", display: "10inh"},
+    {name: "Playstation", display: "50inh"}
+  ];
+  console.log(computer);
+  //Ex2:
+  let phone = [
+    {name: "Samsung", country: "Korean"},
+    {name: "Iphone", country: "USA"},
+    {name: "Xiaomi", country: "China"},
+    {name: "Nokia", country: "Finland"}
+  ];
+  console.log(phone);
