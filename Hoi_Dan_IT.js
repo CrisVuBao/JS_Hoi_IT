db49707b1452d6dbad5778d4b185f3449c542c88
Bao <vuthebao773@gmail.com>
2023-01-05 13:58:15 +0700

    Video_#15 loop for()

--- Hoi_Dan_IT.js
+++ Hoi_Dan_IT.js
@@ -1,13 +1,23 @@
-//Video_#14
+let dogs = ['husky','chó cỏ','alaska']
 
-/* Compare:
-  == : compare value
-  === : compare type, value
-*/
-let poundDog = 30;
-let poundCat = 5;
-console.log("Compare:", poundDog === poundCat);
+for(let i = 0; i < dogs.length; i++) {
+  console.log(dogs[i]);
 
-// Empty: Rỗng
-// Underfined: không xác định 
-// Null:  Ex: let a = Null; || let a = '';
+  // in loop
+  // Nên sử dụng let vì phạm vi(scope) ngắn, chỉ trong vòng lặp
+  // phạm vi của var rộng, nên dễ bị sai xót, hiểu lầm.
+  // Ex: for(let a = 5), chỉ trong vòng lặp mới biết a = 5;
+  //     for(var a = 5) {};
+  //   console.log(a); //, khi console.log ở bên ngoài vòng lặp thì vẫn biết a = 5
+}
+
+console.log(i);
+
+// i++ and ++i
+//Ex:
+let i1 = 10;
+let j1 = i1++; // i = 11, j = 10
+console.log(j)  
+//Ex 2:
+let i2 = 10;
+let j2 = ++i2; // i = 11; j = 11;
