52df0c4f7583e4547e2ea84d0f78514fa51000c2
Bao <vuthebao773@gmail.com>
2023-01-06 15:35:17 +0700

    Video_#19 Scope

--- Hoi_Dan_IT.js
+++ Hoi_Dan_IT.js
@@ -1,12 +1,14 @@
-let animals = 1
-switch(animals) {
-  case 0:
-    animals = "cho"
-  case 1:
-    animals = "cho cho"
-  case 2:
-    animals = "vip"
-    break;
-    // khi không có break, thì dù có chọn animal = 1, thì vẫn chạy xuống case 2
+// Scope
+let aPro = 10;
+console.log(aPro); // global scope
+
+function animals(dog,cat) { //function scope
+
+  let aFun = "con Cho"; // function scope 
+
+  let dogPro = { // block scope
+    a : 10,
+    b : 20
+  };
+
 };
-console.log(animals);
