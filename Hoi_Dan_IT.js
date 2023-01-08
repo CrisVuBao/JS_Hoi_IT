257e9c59672b85be3f40dde093cd1da26ce882e2
Bao <vuthebao773@gmail.com>
2023-01-08 17:18:53 +0700

    Video_#23_Func_in_Object

--- Hoi_Dan_IT.js
+++ Hoi_Dan_IT.js
@@ -1,47 +1,12 @@
-// Arrow Function
-/* Ex:
-let sum = (a,b) => {
-    return a + b;
-}
-
-console.log(sum(10,10));
-*/
-
-//Ex 2:
-let getSum = (a,b) => {
-  return a + b;
-}
-console.log(getSum(10,20));
-
-//Ex 3:
-let getName = (firstName,lastName) => {
-  return lastName + ` ` + firstName;
-}
-console.log(getName("bao","vu"));
-
-//Ex 4:
-let getArr = (arr) => {
- arr = ['tao','pro','vip'];
-  return arr;    
-}
-console.log(getArr());
-
-//Ex 5:
-let getLoop = (loop) => {
-  loop = 10;
-  for(let i = 0; i < loop; i++) {
-    console.log(i)
-  }
-}
-console.log(getLoop());
+// Callbacks
 
 
-//Ex Function. Methods
-let family = {
-  name: "cho",
-  age: "10",
-  getDog: function(n1) {
-    return n1
-  }
-}
-console.log(family.getDog("gọi câu này từ function bên trong object"));
+//setTimeOut
+setTimeout(() => {
+  console.log("hello settimeout");
+}, 10000);
+
+//setInterval
+setInterval(() => {
+  // console.log("hello setinterval");
+}, 1000);
