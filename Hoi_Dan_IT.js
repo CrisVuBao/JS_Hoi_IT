14b47db524e57c4209787d684420451b6c02f160
Bao <vuthebao773@gmail.com>
2023-01-10 08:16:48 +0700

    Video_#25_Filter_Find

--- Hoi_Dan_IT.js
+++ Hoi_Dan_IT.js
@@ -1,12 +1,20 @@
-// Callbacks
+let  dogNumber = [
+  {name: 'husky', crazy: 1},
+  {name: 'alaska', crazy: 2},
+  {name: 'cogy', crazy: 3},
+  {name: 'cho ngu', crazy: 3}
+];
+// Filter, Find
+let putFilter = dogNumber.filter((item,index) => {
+  return item &&  item.crazy === 3; // nếu chỉ để mỗi item.crazy === 3 thì 
+  // nó sẽ trả về true hoặc false 
+  // Còn nếu thêm item && , thì nó sẽ cố gắng trả về phần tử trong mảng đã khai báo
+});
+console.log(putFilter);
 
-
-//setTimeOut
-setTimeout(() => {
-  console.log("hello settimeout");
-}, 10000);
-
-//setInterval
-setInterval(() => {
-  // console.log("hello setinterval");
-}, 1000);
+//Find
+let outFind = dogNumber.find((item, index) => { 
+// Find: tìm và xuất ra phần tử đầu tiên trong mảng, object,.vv nếu thỏa điều kiện
+  return item && item.crazy > 1;
+});
+console.log(outFind);
