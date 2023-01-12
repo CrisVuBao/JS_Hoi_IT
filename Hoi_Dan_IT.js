0592e1a9dab0d3ab4d7f9919a33a6b4c13d37781
Bao <vuthebao773@gmail.com>
2023-01-12 21:28:16 +0700

    Video_#28_Soft

--- Hoi_Dan_IT.js
+++ Hoi_Dan_IT.js
@@ -1,15 +1,18 @@
-// Map
-let arr = [1,3,5,7,9,10];
-for(let i = 0; i < arr.length; i++) { // thay đổi dữ liệu, modify
-  arr[i] = arr[i] * arr[i];
-}
-console.log(arr);
+let arrNum = [1,10,5,3];
 
-let arrMap = arr.map((item,index) => { // đây là tạo ra mảng mới từ mảng ban đầu
-  item = item * item;
-  return item;
+let outAN = arrNum.sort((a,b) => { // sort: công dụng là sắp lại theo đúng thứ tự quy định
+  return a - b;
 });
-console.log(arrMap);
+console.log(outAN);
 
-// nếu không muốn thay đổi biến, trả ra mảng mới, thì dùng map
-// muốn tạo ra mảng mới trong biến arr thì dùng map, mảng cũ vẫn như cũ.
+let objName = [
+  {stt:3, name: "mtp"},
+  {stt:7, name: "key trần"},
+  {stt:1, name: "low "},
+  {stt:10, name: "mười"}
+];
+
+let outObj = objName.sort((item1,item2) => { // Ex sort: sắp xếp stt theo thứ tự là "stt: 1,3,7,10"
+  return item1.stt - item2.stt;
+});
+console.log(outObj);
