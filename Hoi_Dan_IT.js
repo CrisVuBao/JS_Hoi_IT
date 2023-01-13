2b1bcd9003c42be63a6787207e2316e2a6198c15
Bao <vuthebao773@gmail.com>
2023-01-13 12:59:23 +0700

    Video_#29 thu vien lodash

--- Hoi_Dan_IT.js
+++ Hoi_Dan_IT.js
@@ -1,18 +1,4 @@
-let arrNum = [1,10,5,3];
-
-let outAN = arrNum.sort((a,b) => { // sort: công dụng là sắp lại theo đúng thứ tự quy định
-  return a - b;
-});
-console.log(outAN);
-
-let objName = [
-  {stt:3, name: "mtp"},
-  {stt:7, name: "key trần"},
-  {stt:1, name: "low "},
-  {stt:10, name: "mười"}
-];
-
-let outObj = objName.sort((item1,item2) => { // Ex sort: sắp xếp stt theo thứ tự là "stt: 1,3,7,10"
-  return item1.stt - item2.stt;
-});
-console.log(outObj);
+// Thư viện lodash: 
+// + Công dụng: giúp cho viết code ngắn gon hơn
+// + Kí tự của lodash: _ (dấu gạch dưới)
+// Ex: _array();
