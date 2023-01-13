bc3a7ed49c2b03a9299b3f9aaa1592b6cb74fb88
CrisMixi <88870890+CrisMixi@users.noreply.github.com>
2023-01-13 15:12:32 +0700

    Update Hoi_Dan_IT.js

--- Hoi_Dan_IT.js
+++ Hoi_Dan_IT.js
@@ -1,16 +1,13 @@
-// Async (Asynchronous): Bất đồng bộ
-// + Ex: ví dụ trong ngôn ngữ java có tính (đồng bộ), khi run code, phải chạy code theo thứ tự.
-//cái nào vào trước thì dc chạy trước
-// + Ex 2: ví trọng trong javascript (bất đồng bộ) , không quan trọng thứ tự, code cứ chạy hết, cứ về đến đích là dc, ra kết quả là dc
+// HTTP Requests: request(yêu cầu) API từ bên page nào đó, để lấy data từ bên page đó
+//cho vào page của mình (gọi là mượn dữ liệu từ nơi khác để phục vụ cho mình).ok
+// Ex:
+var request = new XMLHttpRequest();
 
+request.onreadystatechange = function () {
+  if (this.readyState == 4) {
+    console.log(":)) output: ", request);
+  };
+};
 
-// Ex Code:
-console.log("Chó");
-console.log("Lợn");
-setTimeout( () => {
-  console.log("Hello dog");
-  
-}, 3000);
-console.log("Gao ồ");
-// như dòng code trên về (bất đồng bộ): kết quả vận xuất ra chó, lợn, gao ồ trước
-//rồi sau đó mới đến hello dog, vì bất đồng bộ là cái nào vào trước thì dc ra kết quả trước, ko cần phải xếp hàng đợi, ko cần theo thứ tự
+request.open("Get","https://jsonplaceholder.typicode.com/todos",true);
+request.send();
