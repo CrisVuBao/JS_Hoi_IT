e90e87b67a059739f6c0bbec659c8b3bdd967d04
Bao <vuthebao773@gmail.com>
2023-01-13 14:03:20 +0700

    jsPro_#1 Async

--- Hoi_Dan_IT.js
+++ Hoi_Dan_IT.js
@@ -1,4 +1,16 @@
-// Thư viện lodash: 
-// + Công dụng: giúp cho viết code ngắn gon hơn
-// + Kí tự của lodash: _ (dấu gạch dưới)
-// Ex: _array();
+// Async (Asynchronous): Bất đồng bộ
+// + Ex: ví dụ trong ngôn ngữ java có tính (đồng bộ), khi run code, phải chạy code theo thứ tự.
+//cái nào vào trước thì dc chạy trước
+// + Ex 2: ví trọng trong javascript (bất đồng bộ) , không quan trọng thứ tự, code cứ chạy hết, cứ về đến đích là dc, ra kết quả là dc
+
+
+// Ex Code:
+console.log("Chó");
+console.log("Lợn");
+setTimeout( () => {
+  console.log("Hello dog");
+  
+}, 3000);
+console.log("Gao ồ");
+// như dòng code trên về (bất đồng bộ): kết quả vận xuất ra chó, lợn, gao ồ trước
+//rồi sau đó mới đến hello dog, vì bất đồng bộ là cái nào vào trước thì dc ra kết quả trước, ko cần phải xếp hàng đợi, ko cần theo thứ tự
