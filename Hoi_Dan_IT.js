f50f9c1eead8bd368fe30cff496e9723f0f1f06e
Bao <vuthebao773@gmail.com>
2023-01-15 14:45:20 +0700

    jsPro_#8 Chaining Promises 1

--- Hoi_Dan_IT.js
+++ Hoi_Dan_IT.js
@@ -1,8 +1,5 @@
-// Callback Hell:
-//như kiểu function lồng trong function, thành hình kim tự tháp
-// lý thuyết: lồng xử lý bất đồng bộ của js vào bên trong của nó
+// Chaining Promises
 
-    // $.support.cors = true;
     const callback = (error, data) => {
         if(error) {
             console.log("calling callback with error: ", error);
@@ -12,25 +9,40 @@
         }
     }
 
-function getTodos(callback) {
-    let request = new XMLHttpRequest();
-
-    request.onreadystatechange = function() {
-        if(this.readyState === 4 && request.status === 200) {
+// #8 ********
+function getTodos(id, callback) {
+    
+    return new Promise((resolve, reject) => {
+        let request = new XMLHttpRequest();
+        request.onreadystatechange = function() {
+            if(this.readyState === 4 && request.status === 200) {
             const data = JSON.parse(request.responseText);
             const dataString = JSON.stringify(data)
-            callback(undefined, data);
-            callback(undefined, dataString);
-            callback(undefined, request.responseText);
-        };
+            resolve(data);
+            };
 
-        if(this.readyState === 4 && request.status !== 200) {
-            callback("something wrongs", undefined);
-        };
+            if(this.readyState === 4 && request.status !== 200) {
+            callback("something wrongs ", undefined);
+            };
     };
     request.open("get", `https://jsonplaceholder.typicode.com/todos/${id}`,true);
     request.withCredentials = true;
     request.send();
+    });   
 };
 
-   getTodos(1, callback);
+getTodos(1)
+    .then(data1 => {
+        console.log("get data1: ", data1);
+        return getTodos(2);
+    }).then(data2 => {
+        console.log("get data2: ", data2);
+        return getTodos(3);
+    })
+    .then(data3 => {{
+        console.log("get data3: ", data3);
+    }})
+
+    .catch(err => {
+        console.log("error", err);
+    });
