8b741f09d0bd0c034f7176191245751b95c0a105
Bao <vuthebao773@gmail.com>
2023-01-16 14:09:32 +0700

    jsPro_#11 Throwing Errors

--- Hoi_Dan_IT.js
+++ Hoi_Dan_IT.js
@@ -1,4 +1,4 @@
-// Async & Await
+// Throwing Errors
     const callback = (error, data) => {
         if(error) {
             console.log("calling callback with error: ", error);
@@ -31,20 +31,48 @@ function getTodos(id, callback) {
 
 
 
-// #10 Async & Await 
+// Async & Await 
 // xử lý bất đồng bộ sao dễ vậy
-// * Async
-const getNewTodo = async(id) => {
-   let response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
-   let data = await response.json(); 
-   console.log("get data", data);
-    // nếu console ra promise {pending}: gọi là code chạy xong rồi, nhưng promise chưa trả ra kết quả, nghĩa là: đang tạm ngừng.
+//*** Throwing Errors
+// const getNewTodo = async(id) => {
+//    let response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
+//     if(response && response.status !== 200) {
+//         throw new Error("something wrongs with status code: "+ response.status);
+//         //reject
+//     }
 
+//     let data = await response.json(); 
+//     return data; // resolve
+// }
+
+
+// getNewTodo("hjakfjkasd").then(data => {
+//     console.log("get data: ", data);
+// })
+//     .catch(err => { 
+//         console.log("check error: ", err.message);
+//     });
+
+// *** Try - catch
+const getNewTodo = async (id) => {
+    try {
+        let response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
+        if (response && response.status !== 200) {
+            throw new Error("something wrongs with status code: " + response.status);
+            // reject
+        }
+
+        let data = await response.json();
+        return data; // resolve
+    } catch (e) {
+        console.log("check catch error: ", error.message);
+    };
 }
-console.log(getNewTodo()); // nếu chỉ getNewTodo thôi thì , sẽ là tham chiếu đến hàm async()
-                            // nếu getNewTodo() , đang gọi hàm getNewTodo
 
-getNewTodo(3).then(data => {
-    console.log("get data: ", data);
-});
-
+getNewTodo(1).then(data => {
+    console.log("get data for try catch: ", data);
+})
+    .catch(err => { 
+        console.log("check error try catch: ", err.message);
+    });
+    
